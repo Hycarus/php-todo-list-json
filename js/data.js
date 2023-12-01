1cddb43d35f27c7fdf1fec11930a664ffaa0ed8a
Hycarus <mirkomarasco08@gmail.com>
2023-12-01 14:07:58 +0100

    added old scaffholding

---
+++ js/data.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 1,
+        text: 'Computer',
+        done: false,
+    }, 
+    {
+        id: 2,
+        text: 'Playstation',
+        done: true,
+    }, 
+    {
+        id: 3,
+         text: 'Iphone 14',
+        done: false,
+    },
+    {
+        id: 4,
+        text: 'T-shirt',
+        done: true,
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,67 @@
+import {getIndex} from './utility.js';
+import {tasks} from './data.js';
+
+const {createApp} = Vue
+createApp({
+    // contiene tutti i dati / le variabili 
+    data(){
+        return {
+            tasks: tasks,
+            lastId: 4,
+            todoText: '',
+            filterValue: '',
+            audio: new Audio('audio/jingle-bells.mp3'),
+            active: true,
+        }
+    },
+    // contiene le funzioni e i metodi 
+    methods: {
+        addTask(){
+            this.tasks.unshift({
+                id: ++this.lastId,
+                text: this.todoText,
+                done: false,
+            });
+            this.todoText = '';
+        },
+        removeTask(id){
+            const index = getIndex(id, this.tasks)
+            this.tasks.splice(index, 1);
+        },
+        todoInvert(id){
+            const index = getIndex(id, this.tasks)
+            this.tasks[index].done = !this.tasks[index].done
+        },
+        // getIndex(id){
+        //     return this.tasks.findIndex((el) => el.id === id);
+        // },
+        filteredTasks(){
+            return this.tasks.filter((task)=>{
+                if(this.filterValue === '2' && !task.done){
+                    return true;
+                } else if(this.filterValue === '1' && task.done){
+                    return true;
+                } else if(this.filterValue === ''){
+                    return true;
+                }
+            });
+        },
+        stopMusic(){
+            this.audio.pause();
+            this.active = false;
+        },
+        playMusic(){
+            this.audio.play();
+            this.active = true;
+        },
+    },
+    mounted(){
+        this.audio.play();
+    },
+    // contiene funzioni che possono essere richiamate solo se viene modificato un dato
+    computed: {
+
+    },
+}).mount('#app')
+
+
